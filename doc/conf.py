dcb58501c16a351aa76ceebd12ac6686531b59cf
Toby the potato <tobyclh@gmail.com>
2024-08-08 17:34:44 +0900

    add version

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,5 @@
+import os
+import yaml
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,10 +8,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'FCS 24.07 Manual'
-copyright = 'workshop participant'
+project = 'FCS Manual'
+copyright = 'TOEI Zukun'
 author = 'Zukun'
-release = '24.07'
 
 
 # -- General configuration ---------------------------------------------------
@@ -26,3 +27,48 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+# -- sphinx-intl -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/advanced/intl.html
+locale_dirs = ['locale/']
+gettext_compact = False
+
+# get the environment variable build_all_docs and pages_root
+build_all_docs = os.environ.get("build_all_docs")
+pages_root = os.environ.get("pages_root", "")
+
+# if not there, we dont call this
+if build_all_docs is not None:
+    # we get the current language and version
+    current_language = os.environ.get("current_language")
+    current_version = os.environ.get("current_version")
+
+    # we set the html_context wit current language and version 
+    # and empty languages and versions for now
+    html_context = {
+        'current_language' : current_language,
+        'languages' : ['ja', 'en'],
+        'current_version' : current_version,
+        'versions' : ['24.07'],
+    }
+
+
+    # and we append all versions and langauges accordingly 
+    # we treat t he main branch as latest 
+    if (current_language == 'en'):
+        html_context['versions'].append(['latest', pages_root + '/en'])
+    if (current_language == 'jp'):
+        html_context['versions'].append(['latest', pages_root])
+
+    # and loop over all other versions from our yaml file
+    # to set versions and languages
+    with open("versions.yaml", "r") as yaml_file:
+        docs = yaml.safe_load(yaml_file)
+
+    if (current_version != 'latest'):
+        for language in docs[current_version].get('languages', []):
+            html_context['languages'].append([language, pages_root+'/'+current_version+'/'+language])
+
+    for version, details in docs.items():
+        html_context['versions'].append([version, pages_root+'/'+version+'/'+current_language])
